f30d67678e2a54d73a707aea946db3befc2b6b09
steckleindesign <steckleindesign@gmail.com>
2025-04-21 22:52:39 -0400

    mult module fully pipelined using dual AD and dual B registers

--- mult.sv
+++ mult.sv
@@ -8,14 +8,28 @@
 
 (* use_dsp = "yes" *)
 module mult #(
-    parameter WIDTH = 8
+    parameter WIDTH_A = 8,
+    parameter WIDTH_B = 8
 )(
-    input  logic clk,
-    input  logic [WIDTH-1:0] din_a, din_b,
-    output logic [WIDTH*2-1:0] dout_p
+    input  logic                       clk,
+    input  logic         [WIDTH_A-1:0] din_a,
+    input  logic         [WIDTH_B-1:0] din_b,
+    output logic [WIDTH_A+WIDTH_B-1:0] dout_p
 );
 
+    // Dual AD reg and dual B reg 1st register is inferred
+    // from registering data in modules providing input data
+
+    logic         [WIDTH_A-1:0] din_a2; // Dual AD reg 2
+    logic         [WIDTH_A-1:0] din_b2; // Dual B reg 2
+    logic [WIDTH_A+WIDTH_B-1:0] m_data; // M reg
+
     always_ff @(posedge clk)
-        dout_p <= $signed(din_a) * $signed(din_b);
+    begin
+        din_a2 <= din_a;
+        din_b2 <= din_b;
+        m_data <= $signed(din_a2) * $signed(din_b2);
+        dout_p <= m_data;
+    end
 
 endmodule
